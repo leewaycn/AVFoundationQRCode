2ee7dc14189f64cda669e452db672c030219ca35
leewaycn <leeway.cn@qq.com>
2015-09-04 22:07:17 +0800

    Initial Commit

---
+++ AVFoundationQRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVFoundationQRCode
+//
+//  Created by apple on 15/9/4.
+//  Copyright (c) 2015年 agewnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVFoundationQRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVFoundationQRCode
+//
+//  Created by apple on 15/9/4.
+//  Copyright (c) 2015年 agewnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
